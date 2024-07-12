ed5cfc65fd17c7da5c72e9280a16b6150ed9c297
Amit Kumar <amitkumar790194@gmail.com>
2024-07-12 15:34:06 +0530

    marker pointer issue in map fixed

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,38 +1,46 @@
-import React from 'react'
+import React from "react";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
-import { Stack } from '@chakra-ui/react';
+import { Stack } from "@chakra-ui/react";
 
 import markerIcon from "../assets/marker-icon.png";
+
+// Fix Leaflet's default icon issue
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon,
+  iconUrl: markerIcon,
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
+
 const Map = ({ coordinates }) => {
+  let state = {
+    keyMAP: Math.random(),
+  };
 
-    const marker = new L.icon({ iconUrl: markerIcon });
-    let state = {
-        keyMAP: Math.random(),
-    };
-    return (
-        <>
-            <Stack w={'100%'} h={'67vh'} zIndex={'5'} mt={0}>
-                <MapContainer
-                    key={state.keyMAP}
-                    center={[coordinates.lat, coordinates.lng]}
-                    zoom={18}
-                    style={{ width: '100%', height: '67vh' }}
-                >
-                    <TileLayer
-                        attribution="Google Maps"
-                        url="https://www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}"
-                    >
-                        <Marker
-                            position={[coordinates.lat, coordinates.lng]}
-                            icon={marker}
-                        ></Marker>
-                    </TileLayer>
-                </MapContainer>
-            </Stack>
-        </>
-    )
-}
+  return (
+    <>
+      <Stack w={"100%"} h={"67vh"} zIndex={"5"} mt={0}>
+        <MapContainer
+          key={state.keyMAP}
+          center={[coordinates.lat, coordinates.lng]}
+          zoom={18}
+          style={{ width: "100%", height: "67vh" }}
+        >
+          <TileLayer
+            attribution="Google Maps"
+            url="https://www.google.cn/maps/vt?lyrs=m@189&gl=cn&x={x}&y={y}&z={z}"
+          />
+          <Marker
+            position={[coordinates.lat, coordinates.lng]}
+            icon={new L.Icon({ iconUrl: markerIcon })}
+          />
+        </MapContainer>
+      </Stack>
+    </>
+  );
+};
 
-export default Map
+export default Map;
